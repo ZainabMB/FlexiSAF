98ab8794fac3dcb8cf7367e8de18e0b19f358584
ZainabMB <148768903+ZainabMB@users.noreply.github.com>
2025-05-22 01:25:05 +0100

    Update Book.java

--- WEEK2/Book.java
+++ WEEK2/Book.java
@@ -1,1 +1,31 @@
+package com.example;
 
+public class Book {
+    private String Author;
+    private String Title;
+    public String ISBN;
+    
+    public Book(String Author, String Title, String ISNB){
+        this.Author= Author;
+        this.Title = Title;
+        this.ISBN = ISNB;
+    }
+    
+    public String getAuthor() { 
+        return Author;
+    }
+    
+    public String getTitle() {
+        return Title; 
+    }
+    
+    public String getISBN() { 
+        return ISBN;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + Title + ", Author: " + Author + ", ISBN: " + ISBN;
+    }
+    
+}
